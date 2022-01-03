78ad9fc748e665a0de7e952d4e3e89045e4d752a
Mario-Guilherme <carvalho16car@gmail.com>
2022-01-03 16:46:10 +0800

    fix: conditional

--- server/server.py
+++ server/server.py
@@ -1,54 +1,98 @@
+"""file that works with server."""
+
+
 class Server:
+    """class that handles a server."""
 
     def __init__(self, id, status, cost):
+        """class constructor.
+
+        Keyword arguments:
+        id -- unique identifier.
+        status --  server status 0(off) and 1(on).
+        cost -- server cost.
+        """
         self.id = id
         self.status = status
         self.cost = cost
-    
+
     @property
     def status(self):
+        """get status fuction."""
         return self.__status
 
     @status.setter
     def status(self, _status):
+        """set status fuction."""
         self.__status = _status
 
     @property
     def cost(self):
+        """get cost fuction."""
         return self.__cost
 
     @cost.setter
     def cost(self, cost):
+        """set cost fuction."""
         self.__cost = cost
 
-
     def create_server(self, umax):
-        
-        server = {"id":self.id,
-                "number_user": 0, 
-                "ttask": [],
-                "umax": umax,
-                "status": self.__status,
-                "cost":self.__cost}
-        
+        """create a dictionary.
+
+        Keyword arguments:
+        umax -- maximum number of users on a server
+
+        return:
+            dict:
+                id -- unique identifier.
+                number_user -- current number of users.
+                ttask -- list of ttask.
+                umax -- maximum number of users on a server.
+                status -- status server.
+                cost -- server cost.
+        """
+        server = {
+            "id": self.id,
+            "number_user": 0,
+            "ttask": [],
+            "umax": umax,
+            "status": self.__status,
+            "cost": self.__cost,
+        }
+
         return server
-    
+
     @staticmethod
     def allocate_on_server(server, user_ttask):
-        server["number_user"] = server["number_user"] +1
+        """add ttask in list server.
+
+        Keyword arguments:
+        server -- dict.
+        user_ttask -- ttask.
+        """
+        server["number_user"] = server["number_user"] + 1
         server["ttask"].append(user_ttask)
         return server
 
     def process_task(self, server):
+        """process a task.
 
-        ttask = [task -1 for task in  server["ttask"] if task > 0]
+        Keyword arguments:
+        server -- dict.
+        """
+        ttask = [task - 1 for task in server["ttask"] if task > 1]
         server["number_user"] = len(ttask)
-        server['ttask'] = ttask
-        self.__cost = self.__cost +1
-        server['cost'] = self.__cost
+        server["ttask"] = ttask
+        self.__cost = self.__cost + 1
+        server["cost"] = self.__cost
         return server
 
     def delete_server(self, server):
+        """delete a server.
+
+        Keyword arguments:
+        server -- dict.
+        """
         self.__status = 0
         server["status"] = self.__status
         return server
