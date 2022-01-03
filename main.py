ef389eac037a83f77462288689dbd39a7a2c4625
Mario-Guilherme <carvalho16car@gmail.com>
2022-01-04 05:00:40 +0800

    docs: add documentation

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from os import umask
+"""principal file."""
 from manage import run
 from utils.clean_data import clean_data
 from utils.handle_file import ReadFile
 from utils.transform_data import transform_data
 from loadbalance.loadbalancer import LoadBalancer
 
+
 if __name__ == "__main__":
+    """fuction to execute system."""
     run()
     data = ReadFile().list_content()
     data_clean = clean_data(data)
@@ -13,6 +15,4 @@ if __name__ == "__main__":
     ttask = data_transform[0]
     umax = data_transform[1]
     users = data_transform[2:]
-    print("Os users porra")
-    print(users)
     LoadBalancer(umax, users).balancer(ttask)
