669353baf0dd0798bc8d4abbd47635c2b99534d1
Mario-Guilherme <carvalho16car@gmail.com>
2022-01-07 10:54:39 +0800

    feat: remove file output file

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 """principal file."""
-from manage import run
+from manage import run, dict_config
+
 from utils.clean_data import clean_data
 from utils.handle_file import ReadFile
 from utils.transform_data import transform_data
+
 from loadbalance.loadbalancer import LoadBalancer
+
+import os
 
 
 if __name__ == "__main__":
@@ -15,4 +19,6 @@ if __name__ == "__main__":
     ttask = data_transform[0]
     umax = data_transform[1]
     users = data_transform[2:]
+    if os.path.exists(dict_config["OUTPUT_FILE"]):
+        os.remove(dict_config["OUTPUT_FILE"])
     LoadBalancer(umax, users).balancer(ttask)
