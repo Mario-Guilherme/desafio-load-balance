4b84697230a3eb03c276cbbbb049d02c37c358b3
Mario-Guilherme <carvalho16car@gmail.com>
2022-01-07 11:12:03 +0800

    docs: create documentation

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
+"""configuration system."""
+
 import os
+
 from abc import ABC
 
 from typing import List, Type
@@ -8,23 +11,29 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class BaseConfig(ABC):
+    """Base of Configuration."""
+
     CONFIG_NAME = "base"
     DEBUG = False
 
 
 class DevelopmentConfig(BaseConfig):
+    """Develop configuration."""
+
     CONFIG_NAME = "dev"
     INPUT_FILE = "files/input/input.txt"
-    OUTPUT_FILE = "files/output/output.txt" 
+    OUTPUT_FILE = "files/output/output.txt"
     DEBUG = True
     TESTING = False
     TIME_OUT = 5000
 
 
 class TestingConfig(BaseConfig):
+    """Test configuration."""
+
     CONFIG_NAME = "test"
-    INPUT_FILE = "files/input/input.txt"
-    OUTPUT_FILE = "files/output/output.txt" 
+    INPUT_FILE = "tests/files/test_input/test_file.txt"
+    OUTPUT_FILE = "tests/files/test_output/test_output.txt"
     DEBUG = True
     TESTING = True
     TIME_OUT = 5000
